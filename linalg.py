6802975299e03fcea3db6c318ba65b2328672247
Aquila042 <39123979+Aquila042@users.noreply.github.com>
2018-09-26 10:12:11 +0200

    Added coments and changes to the Galilei method

--- linalg.py
+++ linalg.py
@@ -9,12 +9,14 @@ class TwoVector:
 		self.y=y
 		
 	def angle(self):
-		e1=np.array([1, 0])
-		z=np.array([self.x, self.y])
-		theta=np.arccos((scipy.dot(z, e1))/norm(z))
+		e1=np.array([1, 0]) #creates an unitvector along x 
+		z=np.array([self.x, self.y]) #z is the TwoVector as an array
+		theta=np.arccos((scipy.dot(z, e1))/norm(z)) #here the angle to the x-axis is calculated
 		return theta
 		
-	def Galilei(self, relativevelocity):
-		v0=np.array([1, np.array([self.x, self.y])])
-		vprim=scipy.matmul(np.array([-relativevelocity, 1]), v0)
-		
+	def Galilei(self, relativevelocity): #Relativevelocity is the velocity of S' along the x-axis
+		Svelocity = [self.x, self.y] #velocity in the S system. Denoted v_0 in exersice 1.1
+		transformationmatrix = np.array([[1,0],[-relativevelocity, 1]]) #the transformation matrix gamma
+		V = np.array([1, Svelocity]) #the untransforemed vector
+		vprim=scipy.matmul(transformationmatrix, V)
+		return vprim
